130a2b9bd237b6f83fe562cd9290efa62d0d9582
Valentin Gautier <valentin.gautier@creatis.insa-lyon.fr>
2021-10-26 11:15:15 +0200

    adding TV regularized EM to reconstruction types

--- src/occipet/reconstruction.py
+++ src/occipet/reconstruction.py
@@ -107,3 +107,71 @@ def MLEM(y: np.ndarray, image_shape: np.ndarray,
         image = image * update
 
     return image
+
+
+def div_2d(q: list) -> np.ndarray:
+    """ Computes the divergence of a 2D vector field
+
+    :param q: 2D vector field represented as a list of two 2D matrices. (Ax(x,y), Ay(x,y))
+    :type q: list
+    :returns: The divergence of this vector field represented as a 2D ndarray.
+
+    """
+    grad1=np.gradient(q[0])
+    grad2=np.gradient(q[1])
+    return grad1[0]+grad2[1]
+
+
+def EMTV(y: np.ndarray, image_shape: np.ndarray,
+         nb_iterations: int, projector_id: int,
+         alpha: float=0.75) -> np.ndarray:
+
+    """ TV regularized MLEM from Swatzky et al, Accurate EM-TV algorithm in
+     PET with low SNR
+
+    :param y: sinogram to reconstruct
+    :type y: np.ndarray
+    :param image_shape: shape of the image to be reconstructed
+    :type image_shape: np.ndarray
+    :param nb_iterations: number of titerations for the algorithm
+    :type nb_iterations: int
+    :param projector_id: id of the projector for this sinogram
+    :type projector_id: int
+    :param alpha: weight of the TV regularization
+    :type alpha: float
+    :returns: the reconstructed image
+
+    """
+
+    image = np.ones(image_shape)
+    _, s = back_projection(np.ones(y.shape), projector_id)
+    assert s.shape == image_shape, \
+        "The image shape is different from the one specified by the projector"
+
+    for _ in range(nb_iterations):
+        # wn corresponds to alpha_tilde * uk in the paper
+        wn=alpha*div_zer(image, s)
+        norm=np.max(wn)
+
+        #computing lambda(n+1/2) with a EM step
+        _, ybar = forward_projection(image, projector_id)
+        ratio = div_zer(y, ybar)
+        _, update = back_projection(ratio, projector_id)
+        update = div_zer(update, s)
+        image = image * update
+
+        tau = 1/(4*norm)
+
+        g=[np.zeros(image_shape)]*2
+
+        #computing lambda(n+1) with a weighted TV step
+        for _ in range(175):
+            #phi_int=[g[0],g[1]]
+            div=div_2d(g)
+            grad=np.gradient(wn*div-image)
+            denom=1+tau*np.sqrt(grad[0]**2+grad[1]**2)
+            g[0]=(g[0]+tau*grad[0])/denom
+            g[1]=(g[1]+tau*grad[1])/denom
+
+        image=image-wn*div_2d(g)
+    return image
