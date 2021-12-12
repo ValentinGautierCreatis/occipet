b87a5b53c3c9ebee871d9bc462f563fcc95440d8
Valentin Gautier <valentin.gautier@creatis.insa-lyon.fr>
2021-12-12 11:39:04 +0100

    creating data for reconstruction

--- src/occipet/load_data.py
+++ src/occipet/load_data.py
@@ -4,7 +4,9 @@ Module used to load data
 
 import nibabel as nib
 import numpy as np
-
+import brainweb
+from scipy.fft import fft2, fftshift
+from .utils import create_projector, forward_projection
 
 def load_mnc(path: str) -> np.ndarray:
     """ Load data from a mnc file
@@ -16,3 +18,30 @@ def load_mnc(path: str) -> np.ndarray:
     """
     img = nib.load(path)
     return img.get_fdata()
+
+
+def generate_t1_mr_data(data_file, noise_ratio, noise_sigma):
+
+    slices = (80,slice(120,230),slice(120,230))
+    raw_data = brainweb.load_file(data_file)
+    _,_,t1,_ = brainweb.toPetMmr(raw_data, "mMr", brainweb.FDG)
+    t1 = t1[slices]
+    t1_noisy = brainweb.noise(t1, noise_ratio, noise_sigma)
+    transformed_t1 = fftshift(fft2(t1_noisy))
+
+    return transformed_t1, t1, t1_noisy
+
+
+def generate_pet_data(data_file, noise_ratio, noise_sigma):
+
+    slices = (80,slice(120,230),slice(120,230))
+    raw_data = brainweb.load_file(data_file)
+    pet, *_ = brainweb.toPetMmr(raw_data, "mMr", brainweb.FDG)
+    pet = pet[slices]
+    pet_noisy = brainweb.noise(pet, noise_ratio, noise_sigma)
+
+    angles = np.arange(0, 2*np.pi, 0.05)
+    projector_id = create_projector(pet_noisy.shape, angles, None)
+    _, proj = forward_projection(pet_noisy, projector_id)
+
+    return proj, pet, pet_noisy, projector_id
