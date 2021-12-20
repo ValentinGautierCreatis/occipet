63d31e67b46412fd0ce027135eda76104416d2e3
Valentin Gautier <valentin.gautier@creatis.insa-lyon.fr>
2021-12-20 10:52:19 +0100

    [correction] white gaussian noise on signal measurements

--- src/occipet/load_data.py
+++ src/occipet/load_data.py
@@ -20,16 +20,28 @@ def load_mnc(path: str) -> np.ndarray:
     return img.get_fdata()
 
 
-def generate_t1_mr_data(data_file, noise_ratio, noise_sigma):
+def generate_t1_mr_data(data_file, noise_ratio):
 
     slices = (80,slice(120,230),slice(120,230))
     raw_data = brainweb.load_file(data_file)
     _,_,t1,_ = brainweb.toPetMmr(raw_data, "mMr", brainweb.FDG)
     t1 = t1[slices]
-    t1_noisy = brainweb.noise(t1, noise_ratio, noise_sigma)
-    transformed_t1 = fftshift(fft2(t1_noisy))
+    transformed_t1 = fftshift(fft2(t1))
 
-    return transformed_t1, t1, t1_noisy
+    signal_power = sum(abs(transformed_t1)**2)/transformed_t1.size
+    signal_power_db = 10*np.log10(signal_power)
+
+    noise_power_db = signal_power_db - noise_ratio
+    noise_power = 10**(noise_power_db/10)
+
+    noise_real = np.random.normal(0, 1, transformed_t1.shape)*np.sqrt(noise_power/2)
+    noise_imaginary = np.random.normal(0, 1, transformed_t1.shape)*np.sqrt(noise_power/2)
+
+    noise = noise_real + 1j*noise_imaginary
+
+    noisy_signal = transformed_t1 + noise
+
+    return t1, noisy_signal
 
 
 def generate_pet_data(data_file, noise_ratio, noise_sigma):
