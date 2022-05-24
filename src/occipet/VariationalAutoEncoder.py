ec8b5b683676d151ecf20090eb9f7dceb1906f72
Valentin Gautier <valentin.gautier@creatis.insa-lyon.fr>
2022-05-24 10:48:18 +0200

    Variational autoencoder working on MNIST

--- src/occipet/VariationalAutoEncoder.py
+++ src/occipet/VariationalAutoEncoder.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from __future__ import absolute_import, division, print_function, unicode_literals
+from tensorflow import keras
 from tensorflow.keras import layers
 
 import tensorflow as tf
@@ -12,9 +13,8 @@ class Sampling(layers.Layer):
 
   def call(self, inputs):
     z_mean, z_log_var = inputs
-    batch = tf.shape(z_mean)[0]
-    dim = tf.shape(z_mean)[1]
-    epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
+    dim = tf.shape(z_mean)
+    epsilon = tf.keras.backend.random_normal(shape=dim)
     return z_mean + tf.exp(0.5 * z_log_var) * epsilon
 
 
@@ -23,17 +23,24 @@ class Encoder(layers.Layer):
 
   def __init__(self,
                latent_dim=32,
-               intermediate_dim=64,
                name='encoder',
                **kwargs):
     super(Encoder, self).__init__(name=name, **kwargs)
-    self.dense_proj = layers.Dense(intermediate_dim, activation='relu')
+    self.conv = keras.Sequential([
+        layers.Conv2D(
+              filters=32, kernel_size=3, strides=(2, 2), activation='relu'),
+        layers.Conv2D(
+              filters=64, kernel_size=3, strides=(2, 2), activation='relu')
+      ]
+    )
+    self.flatten = layers.Flatten()
     self.dense_mean = layers.Dense(latent_dim)
     self.dense_log_var = layers.Dense(latent_dim)
     self.sampling = Sampling()
 
   def call(self, inputs):
-    x = self.dense_proj(inputs)
+    x = self.conv(inputs)
+    x = self.flatten(x)
     z_mean = self.dense_mean(x)
     z_log_var = self.dense_log_var(x)
     z = self.sampling((z_mean, z_log_var))
@@ -45,16 +52,26 @@ class Decoder(layers.Layer):
 
   def __init__(self,
                original_dim,
-               intermediate_dim=64,
                name='decoder',
                **kwargs):
     super(Decoder, self).__init__(name=name, **kwargs)
-    self.dense_proj = layers.Dense(intermediate_dim, activation='relu')
-    self.dense_output = layers.Dense(original_dim, activation='sigmoid')
+    self.dense = layers.Dense(original_dim[0]//4 * original_dim[1]//4 * 32)
+    self.reshape =  layers.Reshape(target_shape=(original_dim[0]//4, original_dim[1]//4, 32))
+    self.deconv = keras.Sequential([
+      layers.Conv2DTranspose(
+        filters=64, kernel_size=3, strides=2, padding='same',
+        activation='relu'),
+      layers.Conv2DTranspose(
+        filters=32, kernel_size=3, strides=2, padding='same',
+        activation='relu'),
+      layers.Conv2DTranspose(filters=original_dim[2], kernel_size=3, strides=1, padding='same')
+    ])
 
   def call(self, inputs):
-    x = self.dense_proj(inputs)
-    return self.dense_output(x)
+    x = self.dense(inputs)
+    x = self.reshape(x)
+
+    return self.deconv(x)
 
 
 class VariationalAutoEncoder(tf.keras.Model):
@@ -62,25 +79,54 @@ class VariationalAutoEncoder(tf.keras.Model):
 
   def __init__(self,
                original_dim,
-               intermediate_dim=64,
                latent_dim=32,
                name='autoencoder',
                **kwargs):
     super(VariationalAutoEncoder, self).__init__(name=name, **kwargs)
     self.original_dim = original_dim
-    self.encoder = Encoder(latent_dim=latent_dim,
-                           intermediate_dim=intermediate_dim)
-    self.decoder = Decoder(original_dim, intermediate_dim=intermediate_dim)
+    self.encoder = Encoder(latent_dim=latent_dim)
+    self.decoder = Decoder(original_dim)
+    self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
+    self.reconstruction_loss_tracker = keras.metrics.Mean(
+        name="reconstruction_loss"
+    )
+    self.kl_loss_tracker = keras.metrics.Mean(name="kl_loss")
 
   def call(self, inputs):
     # self._set_inputs(inputs)
     z_mean, z_log_var, z = self.encoder(inputs)
     reconstructed = self.decoder(z)
     # Add KL divergence regularization loss.
-    kl_loss = - 0.5 * tf.reduce_mean(
-        z_log_var - tf.square(z_mean) - tf.exp(z_log_var) + 1)
-    self.add_loss(kl_loss)
+    # kl_loss = - 0.5 * tf.reduce_sum(
+    #     z_log_var - tf.square(z_mean) - tf.exp(z_log_var) + 1)
+    # self.add_loss(kl_loss)
     return reconstructed
+
+  def train_step(self, data):
+      with tf.GradientTape() as tape:
+          # data = tf.convert_to_tensor(data)
+          x, _ = data
+          z_mean, z_log_var, z = self.encoder(x)
+          reconstruction = self.decoder(z)
+          reconstruction_loss = tf.reduce_mean(
+              tf.reduce_sum(
+                  keras.losses.mean_squared_error(x, reconstruction), axis=(1, 2)
+              )
+          )
+          kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
+          kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
+          total_loss = reconstruction_loss + kl_loss
+
+      grads = tape.gradient(total_loss, self.trainable_weights)
+      self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+      self.total_loss_tracker.update_state(total_loss)
+      self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+      self.kl_loss_tracker.update_state(kl_loss)
+      return {
+          "total_loss": self.total_loss_tracker.result(),
+          "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+          "kl_loss": self.kl_loss_tracker.result(),
+      }
 
 
 # original_dim = 784
