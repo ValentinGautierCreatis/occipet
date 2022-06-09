83e2815515368596b2fcc4f2e1be277f5f47fe0c
Valentin Gautier <valentin.gautier@creatis.insa-lyon.fr>
2022-06-09 09:12:16 +0200

    Corrections for the algorithm

--- src/occipet/deep_latent_reconstruction.py
+++ src/occipet/deep_latent_reconstruction.py
@@ -9,6 +9,7 @@ import astra
 import tensorflow as tf
 from . import VariationalAutoEncoder
 
+# TODO: Mettre les Decoder(z) au bon format (tel quel ce sont des tensor shape (1, w, h, 2))
 class DeepLatentReconstruction():
     def __init__(self, y_pet, y_mri, rho, step_size, projector_id,
                  autoencoder: VariationalAutoEncoder.VariationalAutoEncoder,
@@ -39,31 +40,25 @@ class DeepLatentReconstruction():
                                               + self.rho*(self.autoencoder.decoder(z)) - mu)
 
 
-    # TODO: les dimensions sont complètements fausses sur z
-    # c'est pas une image. Généraliser les dimensions de z ?
+    # DONE: z est reçu sous la forme de Tensor comme ça
+    # pas besoin de le reconvertir dans l'algo
     def z_step(self, x, z, mu):
 
-        h, w = z.shape
-        z_tf = tf.Variable(z.reshape((1, h, w, 1)))
+        x = tf.Variable(x.reshape((1,) + x.shape))
+        mu = tf.Variable(mu.reshape((1,) + mu.shape))
         with tf.GradientTape() as tape:
 
-            tape.watch(z_tf)
+            tape.watch(z)
             # careful with the shape z and decoded
-            decoded = self.autoencoder.decoder(z_tf)
+            decoded = self.autoencoder.decoder(z)
+            squared = tf.math.multiply(decoded, decoded)
+            product = tf.math.multiply(x + mu, decoded)
 
-        jacobian = tape.jacobian(decoded, z_tf).numpy()
-        jacobian = jacobian.reshape(jacobian.shape[1], jacobian.shape[2],
-                                    jacobian.shape[5], jacobian.shape[6])
+        squared_gradient = (-1/2) * tape.gradient(squared, z)
+        product_gradient = tape.gradient(product, z)
 
 
-        lagrangian_multiplier = self.rho * (x + mu - decoded)
-
-        product_jacobian = np.empty_like(jacobian)
-        for i in range(h):
-            for j in range(w):
-                product_jacobian[:, :, i, j] = jacobian[:, :, i, j] * lagrangian_multiplier
-
-        update_term = np.sum(product_jacobian, axis=(2,3))
+        update_term = self.rho * (squared_gradient + product_gradient)
 
         return z - self.S*update_term
 
@@ -73,5 +68,21 @@ class DeepLatentReconstruction():
         return mu + x - self.autoencoder.decoder(z)
 
 
-    def reconstruct(self, x0, mu0):
-        z0 =
+    def reconstruct(self, x0, mu0, nb_iterations):
+        z = self.autoencoder.encoder(x0.reshape((1,) + x0.shape))
+        x = x0
+        mu = mu0
+        for _ in range(nb_iterations):
+
+            x_pet = x0[:, :, 0]
+            new_x_pet = self.pet_step(x_pet, z, mu)
+            new_x_pet = new_x_pet.reshape(new_x_pet.shape + (1,))
+            new_x_mr = self.MR_step(z, mu)
+            new_x_mr = new_x_mr.reshape(new_x_mr.shape + (1,))
+            x = np.concatenate((new_x_pet, new_x_mr), axis = 2)
+
+            z = self.z_step(x, z, mu)
+
+            mu = self.lagragian_step(x, z, mu)
+
+        return x
