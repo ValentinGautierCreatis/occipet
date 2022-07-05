b6fda8094630a195ada827d6f6dfe213dbf2575a
Valentin Gautier <valentin.gautier@creatis.insa-lyon.fr>
2022-07-05 11:26:54 +0200

    loading checkpoint properly

--- src/deep_learning/scripts.py
+++ src/deep_learning/scripts.py
@@ -5,9 +5,8 @@ import pathlib
 
 from .variational_auto_encoder import VariationalAutoEncoder
 
-def train_model(model: tf.keras.Model, checkpoint_dir: str,
+def train_model(model: tf.keras.Model, checkpoint_path: str,
                 inputs: np.ndarray, labels: np.ndarray) -> None:
-    checkpoint_path = pathlib.Path(checkpoint_dir) / "cp.ckpt"
     cp_callback = tf.keras.callbacks.ModelCheckpoint(
         filepath=checkpoint_path,
         # save_weights_only=True,
@@ -29,9 +28,9 @@ def train_vae(checkpoint_dir: str,
     model = VariationalAutoEncoder((256, 256, 2))
     optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
     model.compile(optimizer=optimizer, loss=tf.keras.losses.MeanSquaredError())
-    checkpoint_path = pathlib.Path(checkpoint_dir)
+    checkpoint_path = pathlib.Path(checkpoint_dir) / "cp.ckpt"
     data = np.load(data_path)
     model(data[:1,:,:,:])
     if checkpoint_path.exists():
-        model.load_weights(checkpoint_dir)
-    train_model(model, checkpoint_dir, data, data)
+        model.load_weights(checkpoint_path)
+    train_model(model, str(checkpoint_path.resolve()), data, data)
