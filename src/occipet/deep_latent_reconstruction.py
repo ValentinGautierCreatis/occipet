7c6f988ec09c91ff8cdc972e68fc405523309b9f
Valentin Gautier <valentin.gautier@creatis.insa-lyon.fr>
2023-02-06 11:20:18 +0100

    Formatting with Black

--- src/occipet/deep_latent_reconstruction.py
+++ src/occipet/deep_latent_reconstruction.py
@@ -10,8 +10,10 @@ import tensorflow as tf
 from deep_learning import variational_auto_encoder
 
 
-class DeepLatentReconstruction():
-    def __init__(self, autoencoder: variational_auto_encoder.VariationalAutoEncoder) -> None:
+class DeepLatentReconstruction:
+    def __init__(
+        self, autoencoder: variational_auto_encoder.VariationalAutoEncoder
+    ) -> None:
         self.autoencoder = autoencoder
         self.xi = 1
         self.u = 10
@@ -24,16 +26,17 @@ class DeepLatentReconstruction():
         self.y_mr: np.ndarray
         self.projector_id: int
 
-
     def pet_step(self, current, pet_decoded, mu):
         _, sensitivity = back_projection(np.ones(self.y_pet.shape), self.projector_id)
         x_em = em_step(self.y_pet, current, self.projector_id, sensitivity)
-        square_root_term = ( (pet_decoded - mu - (sensitivity/self.rho_pet))**2
-                             + (4 * x_em * sensitivity)/self.rho_pet)
-        new_xpet = 0.5 * (pet_decoded - mu - (sensitivity/self.rho_pet) + np.sqrt(square_root_term))
+        square_root_term = (pet_decoded - mu - (sensitivity / self.rho_pet)) ** 2 + (
+            4 * x_em * sensitivity
+        ) / self.rho_pet
+        new_xpet = 0.5 * (
+            pet_decoded - mu - (sensitivity / self.rho_pet) + np.sqrt(square_root_term)
+        )
 
         return new_xpet
-
 
     def z_step(self, z, x, mu):
         with tf.GradientTape() as tape:
@@ -45,50 +48,43 @@ class DeepLatentReconstruction():
 
         return z - self.step_size * update_term
 
-
     def lagrangian_step(self, x, mu, decoded):
         return mu + x - decoded
 
-
     def compute_residuals(self, x, mu, decoded, old_decoded):
         normalisation_primal = np.max([np.linalg.norm(x), np.linalg.norm(decoded)])
-        norm_primal = np.linalg.norm( (x - decoded)/normalisation_primal )
+        norm_primal = np.linalg.norm((x - decoded) / normalisation_primal)
 
         normalisation_dual = np.linalg.norm(mu)
-        norm_dual = np.linalg.norm((decoded - old_decoded)/normalisation_dual)
+        norm_dual = np.linalg.norm((decoded - old_decoded) / normalisation_dual)
 
         return norm_primal, norm_dual
 
-
     def tau_update(self, norm_primal, norm_dual):
-        update_coeff = np.sqrt(norm_primal/(norm_dual*self.xi))
+        update_coeff = np.sqrt(norm_primal / (norm_dual * self.xi))
         if 1 <= update_coeff < self.tau_max:
             return update_coeff
-        elif (1/self.tau_max) < update_coeff < 1:
-            return 1/update_coeff
+        elif (1 / self.tau_max) < update_coeff < 1:
+            return 1 / update_coeff
         else:
             return self.tau_max
-
 
     def compute_rho_update_factor(self, norm_primal, norm_dual):
         update_factor = 1
         if norm_primal > self.xi * self.u * norm_dual:
             # rho = min(tau * rho, rho_max)
             update_factor = self.tau
-        elif norm_dual > (self.u/self.xi) * norm_primal:
+        elif norm_dual > (self.u / self.xi) * norm_primal:
             # rho = min(rho/tau, rho_max)
-            update_factor = 1/self.tau
+            update_factor = 1 / self.tau
 
         return update_factor
 
-
     def compute_coeffs(self, x, decoded):
-        return x.mean(axis=(0,1))/decoded.mean(axis=(0,1))
-
+        return x.mean(axis=(0, 1)) / decoded.mean(axis=(0, 1))
 
     def decoding(self, z):
         return np.squeeze(self.autoencoder.decoder(z).numpy(), axis=0)
-
 
     def pet_reconstruction_step(self, x, mu, z):
         decoded = self.decoding(z)
@@ -98,123 +94,121 @@ class DeepLatentReconstruction():
         # plt.imshow(x[:,:,1])
         # plt.colorbar()
         # plt.show()
-        pet_decoded = decoded[:,:,0]
-        x_pet = x[:,:,0]
+        pet_decoded = decoded[:, :, 0]
+        x_pet = x[:, :, 0]
 
-        #PET STEP
-        new_x_pet = self.pet_step(x_pet, pet_decoded, mu[:,:,0])
-        new_x = np.concatenate((np.expand_dims(new_x_pet, axis=-1), x[:,:,1:]), axis=-1)
+        # PET STEP
+        new_x_pet = self.pet_step(x_pet, pet_decoded, mu[:, :, 0])
+        new_x = np.concatenate(
+            (np.expand_dims(new_x_pet, axis=-1), x[:, :, 1:]), axis=-1
+        )
 
-        #Z STEP
+        # Z STEP
         new_z = self.z_step(z, new_x, mu)
         new_decoded = self.decoding(new_z)
         new_coeff = self.compute_coeffs(x, new_decoded)
         new_decoded = new_coeff * new_decoded
 
-        #LAGRANGIAN STEP
+        # LAGRANGIAN STEP
         new_mu = self.lagrangian_step(new_x, mu, new_decoded)
 
-        #RESIDUALS
-        norm_primal, norm_dual = self.compute_residuals(new_x[:,:,0], new_mu[:,:,0], new_decoded[:,:,0], decoded[:,:,0])
+        # RESIDUALS
+        norm_primal, norm_dual = self.compute_residuals(
+            new_x[:, :, 0], new_mu[:, :, 0], new_decoded[:, :, 0], decoded[:, :, 0]
+        )
 
-        #TAU UPDATE
+        # TAU UPDATE
         self.tau = self.tau_update(norm_primal, norm_dual)
 
-        #RHO UPDATE
+        # RHO UPDATE
         update_factor = self.compute_rho_update_factor(norm_primal, norm_dual)
         self.rho_pet = self.rho_pet * update_factor
         # Remember to rescale Mu too
-        new_mu[:,:,0] = new_mu[:,:,0]/update_factor
+        new_mu[:, :, 0] = new_mu[:, :, 0] / update_factor
 
-
-        eps_rel = 0.02
         stop = False
-        if norm_primal <= eps_rel and norm_dual <= eps_rel:
+        if norm_primal <= self.eps_rel and norm_dual <= self.eps_rel:
             stop = True
 
         return new_x, new_mu, new_z, stop
 
-
     # Input images with 2 shapes
-    def pet_reconstruction(self, x_pet0, ref_mr, y_pet, projector_id, step_size):
-        #Initializations
-        nb_steps = 40
+    def pet_reconstruction(
+        self, x_pet0, ref_mr, y_pet, projector_id, step_size, nb_steps, eps_rel
+    ):
+        # Initializations
+        self.nb_steps = nb_steps
         self.step_size = step_size
         self.y_pet = y_pet
         self.projector_id = projector_id
-        x_pet0_normalized = x_pet0/x_pet0.max()
-        ref_mr_normalized = ref_mr/ref_mr.max()
+        self.eps_rel = eps_rel
+        x_pet0_normalized = x_pet0 / x_pet0.max()
+        ref_mr_normalized = ref_mr / ref_mr.max()
         x0_normalized = np.concatenate(
             (
                 np.expand_dims(x_pet0_normalized, axis=-1),
-                np.expand_dims(ref_mr_normalized, axis=-1)
+                np.expand_dims(ref_mr_normalized, axis=-1),
             ),
-            axis = -1)
+            axis=-1,
+        )
         *_, z = self.autoencoder.encoder(np.expand_dims(x0_normalized, axis=0))
 
         x = np.concatenate(
-            (
-                np.expand_dims(x_pet0, axis=-1),
-                np.expand_dims(ref_mr, axis=-1)
-            ),
-            axis=-1
+            (np.expand_dims(x_pet0, axis=-1), np.expand_dims(ref_mr, axis=-1)), axis=-1
         )
         mu = np.zeros_like(x)
-        self.rho_pet = 1/np.sum(y_pet)
+        self.rho_pet = 1 / np.sum(y_pet)
 
-
-        #Begining of the algo
-        for _ in range(nb_steps):
+        # Begining of the algo
+        for _ in range(self.nb_steps):
             x, mu, z, stop = self.pet_reconstruction_step(x, mu, z)
             if stop:
                 break
 
-        return x#[:,:,0]
+        return x  # [:,:,0]
 
-    def pet_reconstruction_metrics(self, x_pet0, ref_pet, ref_mr, y_pet, projector_id, step_size):
-        #Initializations
+    def pet_reconstruction_metrics(
+        self, x_pet0, ref_pet, ref_mr, y_pet, projector_id, step_size
+    ):
+        # Initializations
         nb_steps = 40
         self.step_size = step_size
         self.y_pet = y_pet
         self.projector_id = projector_id
-        x_pet0_normalized = x_pet0/x_pet0.max()
-        ref_mr_normalized = ref_mr/ref_mr.max()
+        x_pet0_normalized = x_pet0 / x_pet0.max()
+        ref_mr_normalized = ref_mr / ref_mr.max()
         x0_normalized = np.concatenate(
             (
                 np.expand_dims(x_pet0_normalized, axis=-1),
-                np.expand_dims(ref_mr_normalized, axis=-1)
+                np.expand_dims(ref_mr_normalized, axis=-1),
             ),
-            axis = -1)
+            axis=-1,
+        )
         *_, z = self.autoencoder.encoder(np.expand_dims(x0_normalized, axis=0))
 
         x = np.concatenate(
-            (
-                np.expand_dims(x_pet0, axis=-1),
-                np.expand_dims(ref_mr, axis=-1)
-            ),
-            axis=-1
+            (np.expand_dims(x_pet0, axis=-1), np.expand_dims(ref_mr, axis=-1)), axis=-1
         )
         mu = np.zeros_like(x)
-        self.rho_pet = 1/np.sum(y_pet)
+        self.rho_pet = 1 / np.sum(y_pet)
 
-        #Initializing metrics
+        # Initializing metrics
         list_keys = ["mse", "ssim", "fidelity", "constraint"]
         points_pet = dict((k, list()) for k in list_keys)
 
-        #Begining of the algo
+        # Begining of the algo
         for _ in range(nb_steps):
             x, mu, z, stop = self.pet_reconstruction_step(x, mu, z)
             if stop:
                 break
 
-            #Plots
-            x_pet = x[:,:,0]
+            # Plots
+            x_pet = x[:, :, 0]
             decoded = self.decoding(z)
             decoded = decoded * self.compute_coeffs(x, decoded)
-            points_pet["mse"].append(mse(ref_pet, x_pet/self.coeffs[0]))
-            points_pet["ssim"].append(ssim(x_pet, ref_pet,
-                                        data_range=x_pet.max()))
+            points_pet["mse"].append(mse(ref_pet, x_pet / self.coeffs[0]))
+            points_pet["ssim"].append(ssim(x_pet, ref_pet, data_range=x_pet.max()))
             points_pet["fidelity"].append(data_fidelity_pet(x_pet, y_pet, projector_id))
-            points_pet["constraint"].append(np.sum((x_pet - decoded[:,:,0])**2))
+            points_pet["constraint"].append(np.sum((x_pet - decoded[:, :, 0]) ** 2))
 
-        return x[:,:,0], points_pet
+        return x[:, :, 0], points_pet
--- src/occipet/garage.py
+++ src/occipet/garage.py
@@ -12,10 +12,22 @@ from skimage.metrics import structural_similarity as ssim
 import tensorflow as tf
 from deep_learning.variational_auto_encoder import VariationalAutoEncoder
 
-def dlr(y_pet, projector_id, xpet0, ref_pet, ref_mr, mu0, model, rho, step_size, nb_iterations):
-    xpet0_normalized = xpet0/xpet0.max()
-    x0 = np.concatenate((xpet0, ref_mr), axis = -1)
-    x0_normalized = np.concatenate((xpet0_normalized, ref_mr), axis = -1)
+
+def dlr(
+    y_pet,
+    projector_id,
+    xpet0,
+    ref_pet,
+    ref_mr,
+    mu0,
+    model,
+    rho,
+    step_size,
+    nb_iterations,
+):
+    xpet0_normalized = xpet0 / xpet0.max()
+    x0 = np.concatenate((xpet0, ref_mr), axis=-1)
+    x0_normalized = np.concatenate((xpet0_normalized, ref_mr), axis=-1)
     *_, z = model.encoder(np.expand_dims(x0_normalized, axis=0))
     x = x0
     mu = mu0
@@ -30,27 +42,32 @@ def dlr(y_pet, projector_id, xpet0, ref_pet, ref_mr, mu0, model, rho, step_size,
     list_keys = ["mse", "ssim", "fidelity", "constraint"]
     points_pet = dict((k, list()) for k in list_keys)
 
-    points_pet["mse"].append(mse(np.squeeze(ref_pet, axis=-1), np.squeeze(xpet0, axis=-1)))
+    points_pet["mse"].append(
+        mse(np.squeeze(ref_pet, axis=-1), np.squeeze(xpet0, axis=-1))
+    )
     for _ in range(nb_iterations):
         old_z = np.array(z, copy=True)
         decoded = np.squeeze(model.decoder(z).numpy(), axis=0)
-        coeffs = x.mean(axis=(0,1))/decoded.mean(axis=(0,1))
+        coeffs = x.mean(axis=(0, 1)) / decoded.mean(axis=(0, 1))
         decoded = coeffs * decoded
-        pet_decoded = decoded[:,:,0]
+        pet_decoded = decoded[:, :, 0]
 
         # print(rho)
-        plt.imshow(x[:,:,0])
-        plt.imshow(decoded[:,:,0]/coeffs[0])
+        plt.imshow(x[:, :, 0])
+        plt.imshow(decoded[:, :, 0] / coeffs[0])
         plt.colorbar()
         plt.show()
 
         # PET STEP =========================================
-        x_pet = x[:,:,0]
+        x_pet = x[:, :, 0]
         _, sensitivity = back_projection(np.ones(y_pet.shape), projector_id)
         x_em = em_step(y_pet, x_pet, projector_id, sensitivity)
-        square_root_term = ( (pet_decoded - mu[:,:,0] - (sensitivity/rho))**2
-                             + (4 * x_em * sensitivity)/rho)
-        new_xpet = 0.5 * (pet_decoded - mu[:,:,0] - (sensitivity/rho) + np.sqrt(square_root_term))
+        square_root_term = (pet_decoded - mu[:, :, 0] - (sensitivity / rho)) ** 2 + (
+            4 * x_em * sensitivity
+        ) / rho
+        new_xpet = 0.5 * (
+            pet_decoded - mu[:, :, 0] - (sensitivity / rho) + np.sqrt(square_root_term)
+        )
 
         x = np.concatenate((np.expand_dims(new_xpet, axis=-1), ref_mr), axis=-1)
 
@@ -67,56 +84,62 @@ def dlr(y_pet, projector_id, xpet0, ref_pet, ref_mr, mu0, model, rho, step_size,
 
         # LAGRAGIAN STEP
         decoded = np.squeeze(model.decoder(z).numpy(), axis=0)
-        coeffs = x.mean(axis=(0,1))/decoded.mean(axis=(0,1))
+        coeffs = x.mean(axis=(0, 1)) / decoded.mean(axis=(0, 1))
         decoded = coeffs * decoded
         mu = mu + x - decoded
 
         # RESIDUALS
         #
-        normalisation_primal = np.max([np.linalg.norm(x[:,:,0]), np.linalg.norm(decoded[:,:,0])])
-        norm_primal = np.linalg.norm( (x - decoded)[:,:,0]/normalisation_primal )
+        normalisation_primal = np.max(
+            [np.linalg.norm(x[:, :, 0]), np.linalg.norm(decoded[:, :, 0])]
+        )
+        norm_primal = np.linalg.norm((x - decoded)[:, :, 0] / normalisation_primal)
 
         decoded_zk = np.squeeze(model.decoder(old_z).numpy(), axis=0)
-        coeffs_prev = x.mean(axis=(0,1))/decoded_zk.mean(axis=(0,1))
+        coeffs_prev = x.mean(axis=(0, 1)) / decoded_zk.mean(axis=(0, 1))
         decoded_zk = coeffs_prev * decoded_zk
-        normalisation_dual = np.linalg.norm(mu[:,:,0])
-        norm_dual = np.linalg.norm((decoded - decoded_zk)[:,:,0]/normalisation_dual)
+        normalisation_dual = np.linalg.norm(mu[:, :, 0])
+        norm_dual = np.linalg.norm((decoded - decoded_zk)[:, :, 0] / normalisation_dual)
 
-        #TAU UPDATE
-        update_coeff = np.sqrt(norm_primal/(norm_dual*xi))
+        # TAU UPDATE
+        update_coeff = np.sqrt(norm_primal / (norm_dual * xi))
         if 1 <= update_coeff < tau_max:
             tau = update_coeff
-        elif (1/tau_max) < update_coeff < 1:
-            tau = 1/update_coeff
+        elif (1 / tau_max) < update_coeff < 1:
+            tau = 1 / update_coeff
         else:
             tau = tau_max
 
-        #RHO UPDATE
+        # RHO UPDATE
         update_factor = 1
         if norm_primal > xi * u * norm_dual:
             # rho = min(tau * rho, rho_max)
             rho = rho * tau
             update_factor = tau
-        elif norm_dual > (u/xi) * norm_primal:
+        elif norm_dual > (u / xi) * norm_primal:
             # rho = min(rho/tau, rho_max)
-            rho = rho/tau
-            update_factor = 1/tau
+            rho = rho / tau
+            update_factor = 1 / tau
 
-        mu[:,:,0] = mu[:,:,0]/update_factor
-
+        mu[:, :, 0] = mu[:, :, 0] / update_factor
 
         # STOPPING CRITERION
         eps_rel = 0.02
         if norm_primal <= eps_rel and norm_dual <= eps_rel:
             break
 
-        #PLOTS
+        # PLOTS
         # print(np.mean(np.squeeze(ref_pet, axis=-1)))
-        points_pet["mse"].append(mse(np.squeeze(ref_pet, axis=-1), new_xpet/coeffs[0]))
-        points_pet["ssim"].append(ssim(new_xpet, np.squeeze(ref_pet, axis=-1),
-                                    data_range=new_xpet.max()))
-        points_pet["fidelity"].append(data_fidelity_pet(x[:,:,0], y_pet, projector_id))
-        points_pet["constraint"].append(np.sum((x[:,:,0] - decoded[:,:,0])**2))
+        points_pet["mse"].append(
+            mse(np.squeeze(ref_pet, axis=-1), new_xpet / coeffs[0])
+        )
+        points_pet["ssim"].append(
+            ssim(new_xpet, np.squeeze(ref_pet, axis=-1), data_range=new_xpet.max())
+        )
+        points_pet["fidelity"].append(
+            data_fidelity_pet(x[:, :, 0], y_pet, projector_id)
+        )
+        points_pet["constraint"].append(np.sum((x[:, :, 0] - decoded[:, :, 0]) ** 2))
 
     return x, points_pet
 
@@ -128,7 +151,7 @@ def evaluate():
     index = 670
 
     data = np.load(path_to_data)
-    model = VariationalAutoEncoder((256,256,2), 64)
+    model = VariationalAutoEncoder((256, 256, 2), 64)
     model.load_weights(path_to_model)
 
     nb_iterations = 40
@@ -139,35 +162,52 @@ def evaluate():
     S = 0.001
     ######################
 
-
     pet_image = data[index, :, :, 0]
     mri_image = data[index, :, :, 1]
     mu_2d = np.zeros(np.expand_dims(pet_image, axis=-1).shape)
 
-    y_pet, projector_id = load_data.generate_pet_data_from_image(pet_image, 0.01, 60, nb_photons)
+    y_pet, projector_id = load_data.generate_pet_data_from_image(
+        pet_image, 0.01, 60, nb_photons
+    )
     # y_pet, projector_id = load_data.generate_pet_data_from_image(pet_image, 0.5, 200, 12**6)
 
     y_mr, sigma = load_data.generate_t1_mr_data_from_image(mri_image, 0.03)
 
-    x_init_pet = reconstruction.MLEM(y_pet, pet_image.shape, nb_init_steps, projector_id)
+    x_init_pet = reconstruction.MLEM(
+        y_pet, pet_image.shape, nb_init_steps, projector_id
+    )
     x_init_pet = x_init_pet.reshape(x_init_pet.shape + (1,))
 
     x_init_mr = abs(ifft2(y_mr))
-    x_init_mr = x_init_mr.reshape(x_init_mr.shape + (1,))/x_init_mr.max()
+    x_init_mr = x_init_mr.reshape(x_init_mr.shape + (1,)) / x_init_mr.max()
 
     # test_rho = [1/np.sum(x_init_pet), 0.2]
-    test_rho = [1/np.sum(y_pet), 0.2]
+    test_rho = [1 / np.sum(y_pet), 0.2]
 
     # coeff = x_init_pet.mean()/data[index,:,:,0].mean()
-    x, points_pet = dlr(y_pet, projector_id, x_init_pet, data[index,:,:,:1], data[index,:,:,1:], mu_2d, model, test_rho[0], S*10, nb_iterations)
-    ref_mlem = reconstruction.MLEM(y_pet, x_init_pet.shape[:2], mlem_iterations, projector_id)
-    mse_mlem = utils.mse(data[index,:,:,0], ref_mlem/ref_mlem.mean() * data[index,:,:,0].mean())
+    x, points_pet = dlr(
+        y_pet,
+        projector_id,
+        x_init_pet,
+        data[index, :, :, :1],
+        data[index, :, :, 1:],
+        mu_2d,
+        model,
+        test_rho[0],
+        S * 10,
+        nb_iterations,
+    )
+    ref_mlem = reconstruction.MLEM(
+        y_pet, x_init_pet.shape[:2], mlem_iterations, projector_id
+    )
+    mse_mlem = utils.mse(
+        data[index, :, :, 0], ref_mlem / ref_mlem.mean() * data[index, :, :, 0].mean()
+    )
 
     return x, points_pet, mse_mlem
 
 
 def evaluation(nb_photons, S):
-
     indexes = [670, 25, 74, 127, 294, 425, 696, 731, 521]
     # indexes = [0, 12, 15, 22, 24, 25, 26, 30, 32, 38, 61, 68]
     keys = ["mse", "ssim"]
@@ -179,7 +219,7 @@ def evaluation(nb_photons, S):
     index = 25
 
     data = np.load(path_to_data)
-    model = VariationalAutoEncoder((256,256,2), 64)
+    model = VariationalAutoEncoder((256, 256, 2), 64)
     model.load_weights(path_to_model)
 
     nb_iterations = 40
@@ -187,34 +227,50 @@ def evaluation(nb_photons, S):
     mlem_iterations = 15
 
     for index in indexes:
-
         pet_image = data[index, :, :, 0]
         mri_image = data[index, :, :, 1]
         mu_2d = np.zeros(np.expand_dims(pet_image, axis=-1).shape)
 
-        y_pet, projector_id = load_data.generate_pet_data_from_image(pet_image, 0.01, 60, nb_photons)
+        y_pet, projector_id = load_data.generate_pet_data_from_image(
+            pet_image, 0.01, 60, nb_photons
+        )
         y_mr, sigma = load_data.generate_t1_mr_data_from_image(mri_image, 0.03)
 
-        x_init_pet = reconstruction.MLEM(y_pet, pet_image.shape, nb_init_steps, projector_id)
+        x_init_pet = reconstruction.MLEM(
+            y_pet, pet_image.shape, nb_init_steps, projector_id
+        )
         x_init_pet = x_init_pet.reshape(x_init_pet.shape + (1,))
 
         x_init_mr = abs(ifft2(y_mr))
-        x_init_mr = x_init_mr.reshape(x_init_mr.shape + (1,))/x_init_mr.max()
+        x_init_mr = x_init_mr.reshape(x_init_mr.shape + (1,)) / x_init_mr.max()
 
-        test_rho = [1/np.sum(x_init_pet), 0.2]
+        test_rho = [1 / np.sum(x_init_pet), 0.2]
 
         # coeff = x_init_pet.mean()/data[index,:,:,0].mean()
-        x, p = dlr(y_pet, projector_id, x_init_pet, data[index,:,:,:1], data[index,:,:,1:], mu_2d, model, test_rho[0], S*10, nb_iterations)
-        x = x[:,:,0]/x[:,:,0].max()
+        x, p = dlr(
+            y_pet,
+            projector_id,
+            x_init_pet,
+            data[index, :, :, :1],
+            data[index, :, :, 1:],
+            mu_2d,
+            model,
+            test_rho[0],
+            S * 10,
+            nb_iterations,
+        )
+        x = x[:, :, 0] / x[:, :, 0].max()
         plt.imshow(x)
         plt.show()
-        ref_mlem = reconstruction.MLEM(y_pet, x_init_pet.shape[:2], mlem_iterations, projector_id)
-        ref_mlem = ref_mlem/ref_mlem.max()
-        mse_mlem = utils.mse(data[index,:,:,0], ref_mlem)
-        ssim_mlem = ssim(ref_mlem, data[index,:,:,0], data_range=ref_mlem.max())
+        ref_mlem = reconstruction.MLEM(
+            y_pet, x_init_pet.shape[:2], mlem_iterations, projector_id
+        )
+        ref_mlem = ref_mlem / ref_mlem.max()
+        mse_mlem = utils.mse(data[index, :, :, 0], ref_mlem)
+        ssim_mlem = ssim(ref_mlem, data[index, :, :, 0], data_range=ref_mlem.max())
 
-        metrics_dlr["mse"].append(utils.mse(data[index, :,:,0], x))
-        metrics_dlr["ssim"].append(ssim(x, data[index,:,:,0], data_range=x.max()))
+        metrics_dlr["mse"].append(utils.mse(data[index, :, :, 0], x))
+        metrics_dlr["ssim"].append(ssim(x, data[index, :, :, 0], data_range=x.max()))
 
         metrics_mlem["mse"].append(mse_mlem)
         metrics_mlem["ssim"].append(ssim_mlem)
